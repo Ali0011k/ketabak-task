2737496aece4d7763d756cd052fc72a0f2c68a2f
ali <kly441781@gmail.com>
2023-11-30 13:22:45 +0330

    added pagination to the project

--- books/api/v1/views.py
+++ books/api/v1/views.py
@@ -2,7 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import *
 from books.models import Book
 from books.api.v1.serializers import *
-from books.api.v1.permissions import *
+from books.api.permissions import *
+from books.api.paginations import BookPagination
 
 
 class BookModelViewSet(ModelViewSet):
@@ -11,3 +12,4 @@ class BookModelViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
+    pagination_class = BookPagination
--- orders/api/v1/views.py
+++ orders/api/v1/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import *
-from books.api.v1.permissions import IsAdminOrReadOnly
+from books.api.permissions import IsAdminOrReadOnly
 from orders.api.v1.serializers import BorrowedBooksSerializer
+from orders.api.paginations import BorrowedBooksPagination
 from orders.models import BorrowedBooks
-
 
 class BorrowedBooksModelViewSet(ModelViewSet):
     """a model view set for BorrowedBooks Model"""
@@ -11,6 +11,7 @@ class BorrowedBooksModelViewSet(ModelViewSet):
     queryset = BorrowedBooks.objects.all()
     serializer_class = BorrowedBooksSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    pagination_class = BorrowedBooksPagination
 
     def get_queryset(self):
         user = self.request.user
