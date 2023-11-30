fd3838e9da4781009dbafce9e0645cf0a187849c
ali <kly441781@gmail.com>
2023-11-30 13:26:22 +0330

    BookSerializer has been edited

--- books/api/v1/serializers.py
+++ books/api/v1/serializers.py
@@ -5,8 +5,6 @@ from books.models import Book
 class BookSerializer(serializers.ModelSerializer):
     """book model serializer"""
 
-    short_content = serializers.URLField(source="short_description", read_only=True)
-
     class Meta:
         model = Book
         fields = [
@@ -16,16 +14,4 @@ class BookSerializer(serializers.ModelSerializer):
             "price",
             "description",
             "cover_image",
-            "short_content",
         ]
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        request = self.context.get("request")
-        if request and request != None:
-            if request.parser_context:
-                if request.parser_context.get("kwargs"):
-                    data.pop("short_content")
-                else:
-                    data.pop("description")
-        return data
